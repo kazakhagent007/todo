51b6d4d1660c1f10fbb3d5824fd29b42946fe425
Akzholtay Dagzhanuly <akzholtay.k@gmail.com>
2025-01-29 22:35:46 +0500

    fix: api link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     const password = formData.get('password');
     console.log(email, password);
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API}/auth/login`, {
+      const res = await fetch(`/api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
