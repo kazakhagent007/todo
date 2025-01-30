61816e5f09d8bb1f07b0162381eeef10de80b131
Akzholtay Dagzhanuly <akzholtay.k@gmail.com>
2025-01-31 00:55:31 +0500

    fix: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import { Button } from '@headlessui/react';
 import Link from 'next/link';
+import { cookies } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token')?.value;
+
   return (
     <div className="flex flex-col items-center mt-[100px] gap-4">
       <h2>
         To <span className="text-blue">create</span>/<span className="text-green">update</span>/
         <span className="text-red">delete</span> todo list
       </h2>
-      <Link href="/login" passHref>
+      <Link href={token ? '/list' : '/login'} passHref>
         <Button className="rounded bg-gray-800 py-2 px-4 text-sm text-white data-[hover]:bg-black data-[active]:bg-black">
-          Login
+          Start
         </Button>
       </Link>
     </div>
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -2,31 +2,10 @@
 
 import { useMemo, useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
-import { PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { useAuthContext } from '@/config/providers/AuthProvider';
 import { DropdownMenu } from '@/entities/DropdownMenu';
-
-const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-];
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-];
 
 export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -57,17 +36,19 @@ export function Header() {
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
-        {!isLoggedIn && (
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4">
-            <Link href="/login" className="text-sm/6 font-semibold text-gray-900">
-              Login
-            </Link>
-            <Link href="/register" className="text-sm/6 font-semibold text-gray-900">
-              Register
-            </Link>
-          </div>
-        )}
-        {isLoggedIn && <DropdownMenu />}
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4">
+          {!isLoggedIn && (
+            <>
+              <Link href="/login" className="text-sm/6 font-semibold text-gray-900">
+                Login
+              </Link>
+              <Link href="/register" className="text-sm/6 font-semibold text-gray-900">
+                Register
+              </Link>
+            </>
+          )}
+          {isLoggedIn && <DropdownMenu />}
+        </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
@@ -91,7 +72,7 @@ export function Header() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            {isLoggedIn && (
+            {!isLoggedIn && (
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="py-6">
                   <Link
@@ -111,7 +92,28 @@ export function Header() {
                 </div>
               </div>
             )}
-            {isLoggedIn && <DropdownMenu />}
+            {isLoggedIn && (
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <Link
+                  href="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  {user?.email}
+                </Link>
+                <Link
+                  href="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Profile
+                </Link>
+                <Link
+                  href="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Logout
+                </Link>
+              </div>
+            )}
           </div>
         </DialogPanel>
       </Dialog>
